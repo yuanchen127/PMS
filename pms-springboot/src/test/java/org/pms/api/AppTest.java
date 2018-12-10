d82f1169158103342c44e6651ece7a8371de08e1
yuanchen127 <1091355579@qq.com>
2018-12-10 21:42:35 +0800

    init springboot

--- pms-springboot/src/test/java/org/pms/api/AppTest.java
+++ pms-springboot/src/test/java/org/pms/api/AppTest.java
@@ -3,10 +3,16 @@ package org.pms.api;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Unit test for simple App.
  */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class AppTest extends TestCase {
 	/**
 	 * Create the test case
