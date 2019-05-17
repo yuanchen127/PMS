08b2655ff9687d62e2d3b0083e5831983bb1182a
yuanchen127 <1091355579@qq.com>
2019-05-17 18:09:22 +0800

    动态表sql模板

--- mybaits-plus-demo/src/main/java/org/ike/pms/mybatisplus/mybaitsplusdemo/config/mapper/WithTableMapper.java
+++ mybaits-plus-demo/src/main/java/org/ike/pms/mybatisplus/mybaitsplusdemo/config/mapper/WithTableMapper.java
@@ -3,7 +3,6 @@ package org.ike.pms.mybatisplus.mybaitsplusdemo.config.mapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.core.toolkit.Constants;
 import org.apache.ibatis.annotations.Param;
 
 import java.io.Serializable;
@@ -13,25 +12,25 @@ import java.util.List;
 public interface WithTableMapper<T> extends BaseMapper<T> {
     List<T> listWithTable(@Param(WTConstants.TABLE)String table);
 
-    List<T> listWithTable(@Param(WTConstants.TABLE) String table, @Param(Constants.WRAPPER) QueryWrapper<T> queryWrapper);
+    List<T> listWithTable(@Param(WTConstants.TABLE) String table, @Param(WTConstants.WRAPPER) QueryWrapper<T> queryWrapper);
 
     int saveWithTable(@Param(WTConstants.TABLE)String table,@Param(WTConstants.OBJ) T entity);
 
-    int saveBatchWithTable(@Param(WTConstants.TABLE)String table,@Param(Constants.COLLECTION) Collection<T> list);
+    int saveBatchWithTable(@Param(WTConstants.TABLE)String table,@Param(WTConstants.COLLECTION) Collection<T> list);
 
     int removeByIdWithTable(@Param(WTConstants.TABLE)String table, @Param(WTConstants.ID) Serializable id);
 
-    int removeByIdsWithTable(@Param(WTConstants.TABLE)String table,@Param(Constants.COLLECTION)Collection<? extends Serializable> list);
+    int removeByIdsWithTable(@Param(WTConstants.TABLE)String table,@Param(WTConstants.COLLECTION)Collection<? extends Serializable> list);
 
-    int removeWithTable(@Param(WTConstants.TABLE)String table, @Param(Constants.WRAPPER) QueryWrapper<T> queryWrapper);
+    int removeWithTable(@Param(WTConstants.TABLE)String table, @Param(WTConstants.WRAPPER) QueryWrapper<T> queryWrapper);
 
     int updateWithTable(@Param(WTConstants.TABLE)String table, @Param(WTConstants.OBJ)T entity);
 
-    int updateWithTable(@Param(WTConstants.TABLE)String table, @Param(WTConstants.OBJ)T entity, @Param(Constants.WRAPPER) UpdateWrapper<T> updateWrapper);
+    int updateWithTable(@Param(WTConstants.TABLE)String table, @Param(WTConstants.OBJ)T entity, @Param(WTConstants.WRAPPER) UpdateWrapper<T> updateWrapper);
 
-    int updateBatchByIdWithTable(@Param(WTConstants.TABLE)String table, @Param(Constants.COLLECTION)Collection<T> list);
+    int updateBatchByIdWithTable(@Param(WTConstants.TABLE)String table, @Param(WTConstants.COLLECTION)Collection<T> list);
 
     int saveOrUpdateWithTable(@Param(WTConstants.TABLE)String table, @Param(WTConstants.OBJ)T entity);
 
-    int saveOrUpdateBatchWithTable(@Param(WTConstants.TABLE)String table, @Param(Constants.COLLECTION)Collection<T> list);
+    int saveOrUpdateBatchWithTable(@Param(WTConstants.TABLE)String table, @Param(WTConstants.COLLECTION)Collection<T> list);
 }
