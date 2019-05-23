808d3146f2738dd70ba4a5a12f0cde25af23b2e1
yuanchen127 <1091355579@qq.com>
2019-05-23 11:08:24 +0800

    动态表sql模板

--- mybaits-plus-demo/src/main/java/org/ike/pms/mybatisplus/mybaitsplusdemo/config/mapper/WithTableMapper.java
+++ mybaits-plus-demo/src/main/java/org/ike/pms/mybatisplus/mybaitsplusdemo/config/mapper/WithTableMapper.java
@@ -1,7 +1,6 @@
 package org.ike.pms.mybatisplus.mybaitsplusdemo.config.mapper;
 
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Param;
 
